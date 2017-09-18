6ef7e0d3543c2c76e1de8fd248378a67fb0641b4
drakkein <wfurman@pgs-soft.com>
2017-09-18 14:22:14 +0200

    :tada: Initial commit

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "src\\",
+  "globPatterns": [
+    "**/*.{tsx,scss,ts}"
+  ],
+  "swDest": "build/sw.js",
+  "globIgnores": [
+    "..\\workbox-cli-config.js"
+  ]
+};
